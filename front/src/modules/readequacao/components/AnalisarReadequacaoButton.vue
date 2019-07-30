cdaff04a93a9ca86e4b0174f561928d679605621
fernao <fernao@riseup.net>
2019-07-30 11:25:31 -0300

    [UPDATE] Readequação/análise: corrige ativação do componente analisar #2067

--- front/src/modules/readequacao/components/AnalisarReadequacaoButton.vue
+++ front/src/modules/readequacao/components/AnalisarReadequacaoButton.vue
@@ -23,6 +23,11 @@ export default {
             default: () => {},
         },
     },
+    data() {
+        return {
+            dialog: false,
+        };
+    },
     watch: {
         dialog() {
             if (this.dialog === true) {
